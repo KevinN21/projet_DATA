be789f7077a05b4b420e8aec7c10970e99c17640
KevinN21 <Kevin21.Nguyen@gmail.com>
2017-01-18 16:17:31 +0100

    Rangement

---
+++ DateArt/favoris.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Date'Art</title>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets//css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+<?php require_once('template/header.html') ?>
+
+<main class="container">
+  <section class="col-xs-12">
+    <article class="col-xs-3">
+      <img src="assets/img/louvre.jpg" alt="Photo du musée du Louvre, Paris">
+    </article>
+  </section>
+</main>
+
+
+
+
+
+</body>
+</html>
