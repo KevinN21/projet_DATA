29bedd62f71bbe5c9ceba9de4eddf7120d55814a
KevinN21 <Kevin21.Nguyen@gmail.com>
2017-01-20 09:51:09 +0100

    Revert "pull request"
    This reverts commit 93f24d52b162d7a7009ccf05f5a85bf3d8e2ad1a.

---
+++ DateArt/favoris.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Date'Art</title>
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="assets/css/favoristyle.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+</head>
+
+<body>
+<?php require_once('template/header.html') ?>
+
+<main class="container">
+  <section class="col-xs-12">
+    <article>
+      <figure  class="col-xs-4">
+        <figcaption>
+          <img src="assets/img/louvre.jpg" alt="Photo du musée du Louvre, Paris">
+        </figcaption>
+      </figure>
+    </article>
+    <article>
+      <figure  class="col-xs-4">
+        <figcaption>
+          <img src="assets/img/louvre.jpg" alt="Photo du musée du Louvre, Paris">
+        </figcaption>
+      </figure>
+    </article>
+    <article>
+      <figure  class="col-xs-4">
+        <figcaption>
+          <img src="assets/img/louvre.jpg" alt="Photo du musée du Louvre, Paris">
+        </figcaption>
+      </figure>
+    </article>
+  </section>
+</main>
+
+
+
+
+
+</body>
+</html>
---
+++ DateArt/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Date'Art</title>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets//css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+<?php require_once('template/header.html') ?>
+
+ <img src="assets/img/louvre.jpg" width="800" height="600">
+
+</body>
+</html>
