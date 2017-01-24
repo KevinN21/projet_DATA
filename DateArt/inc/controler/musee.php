9d03d5e50a5b12d5062d557ac0de890ae89017ee
KevinN21 <Kevin21.Nguyen@gmail.com>
2017-01-24 14:18:42 +0100

    add leaflet

--- DateArt/inc/controler/musee.php
+++ DateArt/inc/controler/musee.php
@@ -1,7 +1,12 @@
 ﻿<?php
 if(isset($index)){
-$musee = new musee();
-	$data=$musee->GetById($id);
-	callTemplate("musee",$data);
+
+		$musee = new musee();
+		$data=$musee->GetById($id);
+
+		$pos=explode(", ", $data["coordonnees"]);
+		$data['positionx']=$pos[0];
+		$data['positiony']=$pos[1];
+			callTemplate("musee",$data);
 
 }
